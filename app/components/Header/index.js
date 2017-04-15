4534d80dc045df9f5073f3fcb3394dd7de1da228
Unknown <victls@hotmail.fr>
2017-04-15 06:04:47 +0200

    ADD : liens dans le Header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -5,6 +5,7 @@
 */
 
 import React from 'react';
+import { Link } from 'react-router'
 // import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
@@ -14,6 +15,15 @@ function Header() {
   return (
     <div>
       <FormattedMessage {...messages.header} />
+      <Link to="/">
+        <FormattedMessage {...messages.home} />
+      </Link>
+      <Link to="/features">
+        <FormattedMessage {...messages.features} />
+      </Link>
+      <Link to="/introduction">
+        <FormattedMessage {...messages.introduction} />
+      </Link>
     </div>
   );
 }
--- app/components/Header/messages.js
+++ app/components/Header/messages.js
@@ -10,4 +10,16 @@ export default defineMessages({
     id: 'app.components.Header.header',
     defaultMessage: 'This is the Header component !',
   },
+  home: {
+    id: 'boilerplate.components.Header.home',
+    defaultMessage: 'Home',
+  },
+  features: {
+    id: 'boilerplate.components.Header.features',
+    defaultMessage: 'Features',
+  },
+  introduction: {
+    id: 'boilerplate.components.Header.introduction',
+    defaultMessage: 'Mon petit projet',
+  },
 });
