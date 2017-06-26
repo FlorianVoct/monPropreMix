51f7449494949511fd0ba2af107d30462746bfdc
Unknown <victls@hotmail.fr>
2017-06-26 21:34:20 +0200

    FIX : il faut importer les style du slider !!

--- app/components/SliderComponent/index.js
+++ app/components/SliderComponent/index.js
@@ -7,12 +7,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 // import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
+import { SliderDiv } from './style';
+
 class SliderComponent extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+
   onSliderChange(){
     console.log("changement dans le slider");
   }
@@ -24,18 +29,14 @@ class SliderComponent extends React.PureComponent { // eslint-disable-line react
       <div>
         <FormattedMessage {...messages.header} />
         <p>Un autre truc</p>
-        <div
-          style={{height: 200 + "px"}, {display: "flex"}} >
+        <SliderDiv>
           <Slider
             min={12}
             max={78}
-            onChange={this.onSliderChange}
+            onChange={this.onSliderChange.bind(this)}
             defaultValue={50}
-             >
-             A Slider
-             </Slider>
-
-        </div>
+            />
+        </SliderDiv>
       </div>
     );
   }
